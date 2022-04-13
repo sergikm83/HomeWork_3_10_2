845078ded047372b86f3a16b2fd60ed64d290ac2
sergik.m <sergik.m@outlook.com>
2022-04-13 16:41:31 +0400

    Добавьте файлы проекта.

---
+++ HomeWork_3_10_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork_3_10_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
